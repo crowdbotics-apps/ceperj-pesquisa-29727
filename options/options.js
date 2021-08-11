43014ed492c4e642f25be8a7b187c233e1c835a4
Crowdbotics <team@crowdbotics.com>
2021-08-11 18:49:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ceperj_pesquisa_29727Identifier",
+  url: "https://ceperj_pesquisa_29727Identifier.botics.co",
+  api: "https://ceperj_pesquisa_29727Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
